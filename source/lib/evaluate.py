72dbb8746f049f86d47c25bb6cc7d98aa54ab7d1
Alexey Matveev <>
2019-11-01 10:47:41 +0300

    Прогресс бар для оценки

--- source/lib/evaluate.py
+++ source/lib/evaluate.py
@@ -2,8 +2,11 @@
 Оценка качества модели
 """
 
+import math
+
 import torch
 from pycocotools.cocoeval import COCOeval
+from tqdm import tqdm
 
 
 @torch.no_grad()
@@ -17,44 +20,53 @@ def evaluate(model, dataloader, encoder, cocoGt, is_cuda=False):
 
     ret = []
 
-    for nbatch, data in enumerate(dataloader):
-        imgs = data[0]
-        imgs_id = data[1]
-        imgs_size = data[2]
-        bboxes = data[3]
-        labels = data[4]
+    batches_count = math.ceil(
+        len(dataloader.dataset) / dataloader.batch_size
+    )
 
-        if is_cuda:
-            imgs = imgs.cuda()
+    with tqdm(total=batches_count) as progress_bar:
+        for nbatch, data in enumerate(dataloader):
+            imgs = data[0]
+            imgs_id = data[1]
+            imgs_size = data[2]
+            bboxes = data[3]
+            labels = data[4]
 
-        # Get predictions
-        ploc, plabel = model(imgs)
-        ploc, plabel = ploc.float(), plabel.float()
+            if is_cuda:
+                imgs = imgs.cuda()
 
-        # Handel batch prediction
-        for idx in range(ploc.shape[0]):
-            # ease-of-use for specific predictions
-            ploc_i = ploc[idx, :, :].unsqueeze(0)
-            plabel_i = plabel[idx, :, :].unsqueeze(0)
+            # Get predictions
+            ploc, plabel = model(imgs)
+            ploc, plabel = ploc.float(), plabel.float()
 
-            try:
-                result = encoder.decode_batch(ploc_i, plabel_i, 0.50, 200)[0]
-            except Exception:
-                print("")
-                print("No object detected in idx: {}".format(idx))
-                continue
+            # Handel batch prediction
+            for idx in range(ploc.shape[0]):
+                # ease-of-use for specific predictions
+                ploc_i = ploc[idx, :, :].unsqueeze(0)
+                plabel_i = plabel[idx, :, :].unsqueeze(0)
 
-            htot, wtot = imgs_size[idx]
-            for loc_, label_, prob_ in zip(*result):
-                ret.append([
-                    imgs_id[idx],
-                    loc_[0] * wtot,
-                    loc_[1] * htot,
-                    (loc_[2] - loc_[0]) * wtot,
-                    (loc_[3] - loc_[1]) * htot,
-                    prob_,
-                    inv_map[int(label_)]  # Метка из набора данных
-                ])
+                try:
+                    result = encoder.decode_batch(
+                        ploc_i, plabel_i, 0.50, 200
+                    )[0]
+                except Exception:
+                    print("")
+                    print("No object detected in idx: {}".format(idx))
+                    continue
+
+                htot, wtot = imgs_size[idx]
+                for loc_, label_, prob_ in zip(*result):
+                    ret.append([
+                        imgs_id[idx],
+                        loc_[0] * wtot,
+                        loc_[1] * htot,
+                        (loc_[2] - loc_[0]) * wtot,
+                        (loc_[3] - loc_[1]) * htot,
+                        prob_,
+                        inv_map[int(label_)]  # Метка из набора данных
+                    ])
+
+            progress_bar.update()
 
     ret = np.array(ret).astype(np.float32)
 
